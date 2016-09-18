7b5eca8a0202da51a100f5747baa84df45b13aad
lgmoneda <lg.moneda@gmail.com>
2016-09-18 14:42:17 -0300

    FA refactoring

--- old-tasks/T3_2/src/FrontEngine.py
+++ old-tasks/T3_2/src/FrontEngine.py
@@ -1,11 +1,10 @@
 from Engine import Engine
 
-class BackEngine(Engine):
+class FrontEngine(Engine):
 
 	def __init__(self, name):
-		super(BackEngine, self).__init__(name)
-
+		super(FrontEngine, self).__init__(name)
+	
 	def event1Handler(self, event):
 		self.outputs.append(event["content"])
 		self.eventsList.pop(0)
-		return True
